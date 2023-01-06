5101a4b191cf2e7ef9620ca249170c73611b35b8
Tuấn Phạm <tr.tuanpham@gmail.com>
2023-01-07 01:04:51 +0700

    setup work place + app controller

---
+++ Assets/StardewValleyTestSkillCode/Scripts/SVTSC_AppController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SVTSC_AppController : MonoBehaviour
+{
+    public static SVTSC_AppController Instance;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
